e1c91f4a2592a93242c38a94eb5565641155f0c3
Santosh Singh <ausantoshsingh@gmail.com>
2021-01-30 10:42:59 +0530

    Basic app stucture

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./components/App";
+
+render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("App")
+);
